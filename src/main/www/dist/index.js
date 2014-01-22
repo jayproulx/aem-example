0f8fff8dcb53dc7a4dedfeb9c7a45cba5793ff63
Jay Proulx <jproulx@adobe.com>
2014-01-22 16:17:16 -0500

    Tie light toggle action to a server request. During the transition, set lights = undefined to show a transition state. Delay updating the UI for 1000ms to simulate network lag and show the transition.

--- src/main/www/dist/index.js
+++ src/main/www/dist/index.js
@@ -1,11 +1,57 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"NVYujn":[function(require,module,exports){
+/*
+ * Simulating wait state
+ * ---------------------
+ * To simulate network lag, since this should execute pretty quickly, $timeout is used to provide a bit of a delay
+ * when calling services to show off wait states.  The reason it's implemented in the controllers rather than the
+ * services is that we want to return the promise right away, we'll just delay updating the value until after the timeout.
+ */
+
 angular.module( "index", ["ui.bootstrap"] )
 
-	.controller( 'RoomCtrl', ["$scope", function ( $scope ) {
-		$scope.name = $scope.name || "unknown-room";
+	.value( "lightsOnUrl", "/bin/house/lights/on.json" )
+	.value( "lightsOffUrl", "/bin/house/lights/off.json" )
+	.factory( "LightsService", ["$http", "lightsOffUrl", "lightsOnUrl", function ( $http, lightsOffUrl, lightsOnUrl ) {
+		var service = {};
+
+		service.turnOff = function ( room ) {
+			return $http( {method: "GET", url: lightsOffUrl, params: {"room": room}} );
+		};
+
+		service.turnOn = function ( room ) {
+			return $http( {method: "GET", url: lightsOnUrl, params: {"room": room}} );
+		};
+
+		return service;
+	}] )
+
+	.controller( 'RoomCtrl', ["$scope", "$timeout", "LightsService", function ( $scope, $timeout, LightsService ) {
+		$scope.name = $scope.name || "unknown-room"; // this will eventually be re-set by ng-init
+		$scope.lights = false; // default to off
 
 		$scope.toggleLights = function () {
-			$scope.lights = !$scope.lights;
+			var originalState = $scope.lights,
+				newState = !$scope.lights,
+				serviceMethod = newState ? LightsService.turnOn : LightsService.turnOff;
+
+			// set the lights state to undefined while we're waiting for a response, we'll use this to affect the view
+			// during the transition.
+			$scope.lights = undefined;
+
+			serviceMethod( $scope.name )
+				.success( function ( data, status, headers, config ) {
+					$timeout( function () {
+						$scope.lights = data.lights;
+					}, 1000 );
+				} )
+				.error( function ( data, status, headers, config ) {
+					$timeout( function () {
+						// todo: $emit an error event to handle globally if there's a problem toggling the lights.
+
+						// revert the light setting back to what it was, since we couldn't contact the server.
+						$scope.lights = originalState;
+					}, 1000 )
+				} )
 		};
 	}] );
 },{}],"index/index":[function(require,module,exports){
