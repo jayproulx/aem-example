fb77c5f2bb0783383bf19f04b33649ac8132a498
Jay Proulx <jproulx@adobe.com>
2014-01-22 15:22:45 -0500

    Missed some files in the last commit.

--- src/main/www/dist/index.js
+++ src/main/www/dist/index.js
@@ -1,5 +1,13 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"NVYujn":[function(require,module,exports){
-angular.module( "index", ["ui.bootstrap"] );
+angular.module( "index", ["ui.bootstrap"] )
+
+	.controller( 'RoomCtrl', ["$scope", function ( $scope ) {
+		$scope.name = $scope.name || "unknown-room";
+
+		$scope.toggleLights = function () {
+			$scope.lights = !$scope.lights;
+		};
+	}] );
 },{}],"index/index":[function(require,module,exports){
 module.exports=require('NVYujn');
 },{}]},{},["NVYujn"])
--- src/main/www/js/index.js
+++ src/main/www/js/index.js
@@ -1,1 +1,9 @@
-angular.module( "index", ["ui.bootstrap"] );
+angular.module( "index", ["ui.bootstrap"] )
+
+	.controller( 'RoomCtrl', ["$scope", function ( $scope ) {
+		$scope.name = $scope.name || "unknown-room";
+
+		$scope.toggleLights = function () {
+			$scope.lights = !$scope.lights;
+		};
+	}] );
